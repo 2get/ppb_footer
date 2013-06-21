d522cbb2d4ab942ec6b0e16ea1661b63ec5b672a
mizoR <suzunatsu@yahoo.com>
2013-06-21 19:05:26 +0900

    PCフッタ出力に対応した

--- lib/ppb_footer/view_helpers.rb
+++ lib/ppb_footer/view_helpers.rb
@@ -5,11 +5,79 @@ require 'resolv-replace'
 require 'timeout'
 require 'digest/md5'
 require 'fileutils'
+require 'erb'
+require 'csv'
 
 module PpbFooter
   module ViewHelpers
     CACHE_LIFE = 3600
     TIMEOUT = 3
+
+    def render_pc_footer
+      content = ''
+      first = true
+      group_csv   = 'http://www.paperboy.co.jp/footer/paperboy_footer_group.csv'
+      service_csv = 'http://www.paperboy.co.jp/footer/paperboy_footer.csv'
+
+      begin
+        groups = timeout(TIMEOUT) do
+          open(group_csv, 'r:Shift_JIS') do |file|
+            CSV.parse(file.read.encode('utf-8')).map { |row|
+              if row.length >= 2
+                {
+                  :id   => row[0].to_i,
+                  :text => row[1],
+                }
+              end
+            }.compact
+          end
+        end
+
+        services = timeout(TIMEOUT) do
+          open(service_csv, 'r:Shift_JIS') do |file|
+            CSV.parse(file.read.encode('utf-8')).map { |row|
+              if row.length >= 4
+                {
+                  :title    => row[0],
+                  :url      => row[1],
+                  :text     => row[2],
+                  :group_id => row[3].to_i,
+                }
+              end
+            }.compact
+          end
+        end
+
+        groups = groups.sort_by {|group| group[:id]}.map do |group|
+          group[:services] = services.select do |service|
+            service.has_key?(:group_id) and service[:group_id] == group[:id]
+          end
+          group
+        end
+
+        content = ERB.new(<<HTML).result(binding)
+<% groups.each do |group| %>
+  <dl>
+    <dt><%= group[:text] %></dt>
+    <% group[:services].each do |service| %>
+    <dd><a href="<%= service[:url] %>" target="_blank" title="<%= service[:title] %>"><%= service[:text] %></a></dd>
+    <% end %>
+  </dl>
+<% end %>
+HTML
+      rescue StandardError, TimeoutError => e
+        here        = File.dirname(__FILE__)
+        group_csv   = File.join(here, '../../data/paperboy_footer_group.csv')
+        service_csv = File.join(here, '../../data/paperboy_footer.csv')
+        if first
+          first = false
+          retry
+        end
+      end
+
+      content
+    end
+    module_function :render_pc_footer
 
     def open_with_cache(service, ssl = false)
       FileUtils.mkdir_p(cache_base_path)
